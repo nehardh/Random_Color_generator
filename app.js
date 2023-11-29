96d6f5dc870013b376546d35b9af1986e1df2012
Nehardh Madapuri <136841922+nehardh@users.noreply.github.com>
2023-11-29 08:53:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('button');
+
+btn.addEventListener("click" , function() {
+    let h3 = document.querySelector('h3');
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor
+    let div = document.querySelector('div');
+    div.style.background = randomColor;
+
+    console.log("Color Updated")
+});
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
